ae73df10471952cc787bad01f5bfeb01d2ee1e88
piassa157 <lindaum98@gmail.com>
2021-01-19 22:14:14 -0300

    :octocat: babel simple

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const person = {
+    name: 'Gabriel'
+    age: '19',
+    addres: {
+        city: 'champinas'
+        state: 'SP'
+    }
+}
+
+
+const updatePerson = (person) => {
+    person.name = 'Jobson'
+    person.age = '20'
+}
+
+
+console.log(person)
